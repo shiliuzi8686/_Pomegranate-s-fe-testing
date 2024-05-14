47fd96246a23c3ac5e42768a6c2f1af1f6e30aaa
yuting2.liu <yuting2.liu@ly.com>
2024-05-14 17:19:04 +0800

    feat: init todoList program

---
+++ practice/toDoList/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import todoList from './views/todoList.vue'
+</script>
+
+<template>
+  <div class="root-style">
+    <todoList />
+  </div>
+</template>
+
+<style scoped>
+.root-style {
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  background-color: #121826;
+}
+</style>
