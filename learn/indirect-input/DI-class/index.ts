977684372f58453c08f6d25ad158ef7df60a3d0d
yuting2.liu <yuting2.liu@ly.com>
2024-06-27 17:13:50 +0800

    依赖注入: DI-class 构造函数、方法、属性

--- learn/indirect-input/DI-class/index.ts
+++ learn/indirect-input/DI-class/index.ts
@@ -1,5 +1,13 @@
-import { ReadAndProcessFile } from "./readAndProcessFile";
+import { ReadAndProcessFile, FileReader } from "./readAndProcessFile";
+import { readFileSync } from "fs";
+
+class TestFileReader implements FileReader {
+  read(filePath: string): string {
+    return readFileSync(filePath, "utf-8");
+  }
+}
 
 const result = new ReadAndProcessFile();
+result.setFileReader(new TestFileReader())
 
 console.log(result.run("example.txt"));
--- learn/indirect-input/DI-class/readAndProcessFile.ts
+++ learn/indirect-input/DI-class/readAndProcessFile.ts
@@ -1,9 +1,44 @@
-import { readFileSync } from "fs";
+export interface FileReader {
+  read(filePath: string): string;
+}
+// 构造器
+// export class ReadAndProcessFile {
+//   private _fileReader: FileReader;
+//   constructor(fileReader: FileReader) {
+//     this._fileReader = fileReader;
+//   }
+//   run(filePath: string) {
+//     const content = this._fileReader.read(filePath);
 
+//     return content + "->unit test";
+//   }
+// }
+
+// // 属性
+// export class ReadAndProcessFile {
+//   private _fileReader: FileReader;
+//   run(filePath: string) {
+//     const content = this._fileReader.read(filePath);
+
+//     return content + "->unit test";
+//   }
+//   get fileReader() {
+//     return this._fileReader;
+//   }
+//   set fileReader(fileReader: FileReader) {
+//     this._fileReader = fileReader;
+//   }
+// }
+
+// 方法
 export class ReadAndProcessFile {
+  private _fileReader: FileReader;
   run(filePath: string) {
-    const content = readFileSync(filePath, { encoding: "utf-8" });
+    const content = this._fileReader.read(filePath);
 
     return content + "->unit test";
+  }
+  setFileReader(fileReader: FileReader) {
+    this._fileReader = fileReader;
   }
 }
