c31e93fd8dd1da3ad6b17f94185dcf0438f9d447
yuting2.liu <yuting2.liu@ly.com>
2024-07-04 21:12:26 +0800

    init

--- learn/api-vue3-todo-list/vite.config.ts
+++ learn/api-vue3-todo-list/vite.config.ts
@@ -3,9 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    setupFiles: "./vitest.setup.ts",
-  },
+  // test: {
+  //   setupFiles: "./vitest.setup.ts",
+  // },
   plugins: [vue()],
   server: {
     proxy: {
